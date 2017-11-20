943d69c482a44b62d5b02662d0565131dec1fc4d
Neil Shah <warriors18672@gmail.com>
2017-11-20 01:05:10 -0800

    Cell neighbors
    Cells have neighbors

--- model.py
+++ model.py
@@ -8,9 +8,19 @@ class Cell:
     def __init__(self, x: int, y: int):
         self.location = (x, y)
         self.populated = False
+        self.neighbors = get_neighbors(self.location)
     
     def get_location(self) -> (int, int):
         return self.location
     
     def is_populated(self) -> bool:
         return self.populated
+    
+def get_neighbors(location: (int, int)) -> [Cell]:
+    neighbors = []
+    x, y = location
+    for x_coor in range(x - 1, x + 2):
+        for  y_coor in range(y -1, y + 2):
+            if (x_coor, y_coor) != location:
+                neighbors.append(Cell(x, y))
+    return neighbors
